83fccdef08fe7eb11435387c0422dd2b97e754d5
yang_yang <sdzoyy@163.com>
2020-07-09 15:09:09 +0800

    工程重构

--- cloud-consumer-order80/src/main/java/com/yc/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/yc/controller/OrderController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
